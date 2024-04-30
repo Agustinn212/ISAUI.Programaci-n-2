15027dc40f6c2802cd7e58044b08000cbc65a0ac
Agustinn212 <braneagus2@gmail.com>
2024-04-29 22:25:43 -0300

    Se acutaliza

--- Contador de Palabras.py
+++ Contador de Palabras.py
@@ -1,10 +1,4 @@
-
 Frase = input("Ingrese una frase: ")
-
 Palabras = Frase.split()
-
-# Contar el número de palabras en la frase
 NumeroPalabras = len(Palabras)
-
-# Mostrar el número de palabras en la frase
-print(f"El número de palabras en la frase es:", NumeroPalabras)
+print("El número de palabras en la frase es:", NumeroPalabras)
--- Área de un Triángulo.py
+++ Área de un Triángulo.py
@@ -1,6 +1,4 @@
 Base = float(input("Ingrese la longitud de la base del triángulo: "))
 Altura = float(input("Ingrese la altura del triángulo: "))
-
 Area = (Base * Altura) / 2
-
 print("El área del triángulo es:", Area)
